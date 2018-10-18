300b0501d9a9bb3d4ea27cb2db5f6d5d38726cff
sajadviftech <sajjad.viftech333@gmail.com>
2018-10-18 16:16:54 +0500

    enable network

--- header.php
+++ header.php
@@ -21,17 +21,11 @@
 <div id="wrapper" class="thb-page-transition-<?php echo esc_attr(ot_get_option('page_transition', 'on')); ?>">
 	<?php do_action('thb_mobile_menu'); ?>
 	
-	<!-- Start Side Cart -->
 	<?php do_action( 'thb_side_cart' ); ?>
-	<!-- End Side Cart -->
 	
-	<!-- Start Shop Filters -->
 	<?php do_action( 'thb_shop_filters' ); ?>
-	<!-- End Shop Filters -->
 	
-	<!-- Start Header -->
 	<?php get_template_part( 'inc/templates/header/'.ot_get_option('header_style', 'style1').''); ?>
-	<!-- End Header -->
 	
 	<div role="main">
 		<div class="header-spacer"></div>
